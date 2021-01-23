3d426d08fb56ca8258b95fdd61f228683463e867
zoro0611 <77784841+zoro0611@users.noreply.github.com>
2021-01-24 00:35:18 +0800

    third edition

--- products.py
+++ products.py
@@ -5,12 +5,7 @@ while True:
 	if name == "q":
 		break
 	price = input("請輸入商品價格: ")
-
-	p = []
-	p.append(name) # 9~11行這邊代表用p這個清單將商品名稱跟價格一起打包
-	p.append(price) #或者一次寫完 p = [name, price]
-
-	products.append(p) # 最後將p清單再放入products清單中
+	products.append([name, price]) # 一次寫完省略之前三行寫法
 print(products)
 
 
