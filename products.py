21f7af602f005bcd607c7d3e88e48add0ee46b35
zoro0611 <77784841+zoro0611@users.noreply.github.com>
2021-01-25 21:20:24 +0800

    add read file

--- products.py
+++ products.py
@@ -1,19 +1,28 @@
+# 讀取檔案
 products = []
+with open("products.csv", 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',') #每一行 只要遇到"," 就切割
+		products.append([name, price])
+
+print(products)
 
 while True:
 	name = input("請輸入商品名稱: ")
 	if name == "q":
 		break
 	price = input("請輸入商品價格: ")
+	price = int(price)
 	products.append([name, price]) # 一次寫完省略之前三行寫法
 print(products)
 
+#印出所有購買紀錄
 for p in products:
 	print(p[0], "的價格是", p[1])
 	
-print('abc' + '123') # = 'abc123'
-print('abc' * 3) # = 'abcabcabc'
-
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write("商品,價格\n")
 	for p in products:
