d2f785e7a1e1674c078ad5c74db9ddd112e83d9a
zoro0611 <77784841+zoro0611@users.noreply.github.com>
2021-01-28 19:39:28 +0800

    add check file

--- products.py
+++ products.py
@@ -1,15 +1,23 @@
-# 讀取檔案
+import os #operating system
 products = []
-with open("products.csv", 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',') #每一行 只要遇到"," 就切割
-		products.append([name, price])
+ if os.path.isfile('products.csv'):
+ 	print("YAAA!找到檔案了。")
+ 	with open("products.csv", 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',') #每一行 只要遇到"," 就切割
+			products.append([name, price])
+	print(products)
 
-print(products)
 
-#讓使用者輸入
+ else:
+ 	print('找不到!')	
+
+# 讀取檔案
+
+
+# 讓使用者輸入
 while True:
 	name = input("請輸入商品名稱: ")
 	if name == "q":
@@ -19,11 +27,11 @@ while True:
 	products.append([name, price]) # 一次寫完省略之前三行寫法
 print(products)
 
-#印出所有購買紀錄
+# 印出所有購買紀錄
 for p in products:
 	print(p[0], "的價格是", p[1])
 	
-#寫入檔案
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write("商品,價格\n")
 	for p in products:
